e5133280ad17eb0325684778bff326acd5aa2147
NeelDB <n.badracim@campus.fct.unl.pt>
2024-11-03 17:28:14 +0000

    last tukano commit

--- tukano-webapp/src/main/java/tukano/impl/rest/TukanoApplication.java
+++ tukano-webapp/src/main/java/tukano/impl/rest/TukanoApplication.java
@@ -19,28 +19,27 @@ public class TukanoApplication extends Application {
 	public static final BlobsType BLOBS_TYPE = BlobsType.AZURE_BLOBS;
 	public static final DatabaseType USERS_DB_TYPE = DatabaseType.COSMOS_DB_NOSQL;
 	public static final DatabaseType SHORTS_DB_TYPE = DatabaseType.COSMOS_DB_NOSQL;
-	public static final boolean REDIS_CACHE_ON = true;
+	public static final boolean REDIS_CACHE_ON = false;
 
 
 	/** Service Base Uri */
 
-	//public static final String BASE_URI = "https://scc-60485-60492.azurewebsites.net/rest";
-	public static final String BASE_URI = "https://scc-project-60485.azurewebsites.net/rest";
+	public static final String BASE_URI = "https://scc-60485-60492.azurewebsites.net/rest";
+	//public static final String BASE_URI = "https://scc-project-60485.azurewebsites.net/rest";
 
 
 	/** Blobs Configs */
 
-	//public static final String BLOB_STORAGE_KEY = "DefaultEndpointsProtocol=https;AccountName=scc60492;AccountKey=2lddvpV/kKYzpiUq6yOzg52AyB599d1OyeJQf694VGMrr0UbRjIj6Rp3Ns/bsm7htNWCmmwkcDSl+AStQ1GPyg==;EndpointSuffix=core.windows.net";
-	public static final String BLOB_STORAGE_KEY = "DefaultEndpointsProtocol=https;AccountName=scc60485;AccountKey=tRBfHsTj0Fe+vayowI6sGxu24UuVGf1rjY1p9OIL+0jMOP+P6DKzdXX7XSfbNapuL/2ygbMTRxpF+AStL9Ho9A==;EndpointSuffix=core.windows.net";
+	public static final String BLOB_STORAGE_KEY = "DefaultEndpointsProtocol=https;AccountName=scc60492;AccountKey=2lddvpV/kKYzpiUq6yOzg52AyB599d1OyeJQf694VGMrr0UbRjIj6Rp3Ns/bsm7htNWCmmwkcDSl+AStQ1GPyg==;EndpointSuffix=core.windows.net";
+	//public static final String BLOB_STORAGE_KEY = "DefaultEndpointsProtocol=https;AccountName=scc60485;AccountKey=tRBfHsTj0Fe+vayowI6sGxu24UuVGf1rjY1p9OIL+0jMOP+P6DKzdXX7XSfbNapuL/2ygbMTRxpF+AStL9Ho9A==;EndpointSuffix=core.windows.net";
 
 	/** DB Configs */
 
-	public static final String CONNECTION_URL = "https://cosmos-60485.documents.azure.com:443/";
-	public static final String DB_KEY = "b3MQzL5IUay43ec9YOhrStxS4tzRdEwJz25c2knzdiIksbRlYJIgvHPBAnBxhsZ7gu9NR141WJ2HACDbDYFZ9w==";
+	//public static final String CONNECTION_URL = "https://cosmos-60485.documents.azure.com:443/";
+	//public static final String DB_KEY = "b3MQzL5IUay43ec9YOhrStxS4tzRdEwJz25c2knzdiIksbRlYJIgvHPBAnBxhsZ7gu9NR141WJ2HACDbDYFZ9w==";
 
-	//public static final String CONNECTION_URL = "https://scc-60485-60492.documents.azure.com:443/";
-	//public static final String DB_KEY = "gZGjVKxBMJF8fSwF2s3UBmsfdSk9k1vOZq6ziCkCBBsEJYx9wBr1ZRH4tncG5YYh5fW3hoDv0nSdACDbosz4Fg==";
-
+	public static final String CONNECTION_URL = "https://scc-60485-60492.documents.azure.com:443/";
+	public static final String DB_KEY = "ipgvutkBrJQ8pf9REYSAysyJeJHliX3ghwLt7fHEGNOfsURmU1BkkoEaHcPU6OXEeHIYrAK6QS39ACDbKy7mbA==";
 	public static final String TUKANO_RECOMMENDS = "tukano";
 
 	public TukanoApplication() {
